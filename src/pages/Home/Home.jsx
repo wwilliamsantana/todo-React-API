19d01310ca049182b03250621bc73d3194b8d18c
William Silva <euwilliamsantana@gmail.com>
2022-09-28 10:16:48 -0300

    Page: Home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,25 @@
+import "./styles.css"
+
+export function Home() {
+  
+  return (
+    <div className="container">
+
+      <header>
+        <h1>Lista de presença</h1>
+      </header>
+
+      <input type="text" placeholder="Digite seu nome..."/>
+
+      <button
+        type="button"
+      >
+        Adicionar
+      </button>
+
+
+    </div>
+  )
+}
+
+
